f38bd2ca842d77a5499ad34695e667ae1215babe
sotiriskarageorgopoulos <sotiriskarageorgopoulos@gmail.com>
2020-12-25 13:19:28 +0200

    create the functionality of home component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DataSharingService } from '../services/dataSharing/data-sharing.service';
+import { HomeService } from '../services/home/home.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,15 +10,20 @@ import { DataSharingService } from '../services/dataSharing/data-sharing.service
 export class NavbarComponent implements OnInit {
   @Input()
   nameOfComponent: string;
+  tendersNumber: number;
+  tenders: any;
 
-  constructor(private ds:DataSharingService) { }
+  constructor(private ds:DataSharingService, private hs: HomeService) { }
 
   ngOnInit(): void {
+    this.hs.getTenders().subscribe(t => {
+      this.tenders = t;
+      this.tendersNumber = this.tenders.length;
+    });
   }
 
   ngDoCheck(): void {
     this.nameOfComponent = this.ds.getNameOfComponent();
-    console.log(this.nameOfComponent);
   }
 
 }
